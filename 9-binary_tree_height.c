a77ef1318a03bd0da9903d6ee97586d12724c047
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-12 09:31:55 +0000

    tabs instead of spaces

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,15 +8,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_h = 0, right_h = 0;
+	size_t left_h = 0, right_h = 0;
+	
+	if (tree == NULL)
+		return (0);
+	
+	if (tree->left)
+		left_h = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_h = 1 + binary_tree_height(tree->right);
 
-    if (tree == NULL)
-        return (0);
-
-    if (tree->left)
-        left_h = 1 + binary_tree_height(tree->left);
-    if (tree->right)
-        right_h = 1 + binary_tree_height(tree->right);
-
-    return (left_h > right_h ? left_h : right_h);
+	return (left_h > right_h ? left_h : right_h);
 }
